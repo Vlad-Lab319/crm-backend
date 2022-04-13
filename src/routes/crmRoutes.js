af2d0122a749c93094aa443f89f0055cc3658589
vlad-lab319 <solo15mag@gmail.com>
2022-04-13 12:48:17 -0400

    Add contacts endpoints

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,24 +1,37 @@
+import { 
+  addNewContact,
+  getContacts,
+  getContactByID,
+  updateContact,
+  deleteContact
+ } from '../controllers/crmController.js'
+
 const routes = (app) => {
+  
+  // All contacts endpoints
+  
   app.route('/contact')
+
+    // GET enpoint
     .get((req, res, next) => {
       // middleware
       console.log(`Request from ${req.originalUrl}`);
       console.log(`Request type ${req.method}`);
       next();
-    }, (req, res, next) => {
-      res.send('GET request for contact is successful');
-    })
-    .post((req, res) => {
-      res.send('POST request for contact is successful');
-    })
+    }, getContacts)
+
+    // POST endpoint
+    .post(addNewContact);
+
+  // Specific contacts endpoints
 
   app.route('/contact/:contactID')
-    .put((req, res) => {
-      res.send('PUT request for specific contact ID is successful');
-    })
-    .delete((req, res) => {
-      res.send('DELETE requesst for specific contact ID successful');
-    })
+    // GET contact  
+    .get(getContactByID)
+    // Update contact
+    .put(updateContact)
+    // Delete contact
+    .delete(deleteContact)
 }
 
 export default routes;

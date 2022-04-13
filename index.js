cc5ca745dcd38ea84b8a503a24be495e1f264f27
vlad-lab319 <solo15mag@gmail.com>
2022-04-13 09:52:20 -0400

    Install and connect mongodb, setup bodyparser

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
-
 import routes from "./src/routes/crmRoutes.js";
+import mongoose from "mongoose";
+import bodyParser from 'body-parser';
+
 
 const app = express();
 const PORT = 4000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRM', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 routes(app);
 
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,12 +1,17 @@
 const routes = (app) => {
   app.route('/contact')
-    .get((req, res) => {
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from ${req.originalUrl}`);
+      console.log(`Request type ${req.method}`);
+      next();
+    }, (req, res, next) => {
       res.send('GET request for contact is successful');
     })
     .post((req, res) => {
       res.send('POST request for contact is successful');
     })
-  
+
   app.route('/contact/:contactID')
     .put((req, res) => {
       res.send('PUT request for specific contact ID is successful');
